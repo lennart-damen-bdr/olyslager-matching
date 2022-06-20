305076132047dda62e41d6abe855f41c4ff5ed35
Lennart Damen <lennartdamen@Lennarts-MacBook-Pro.local>
2022-06-20 09:42:42 +0200

    Remove unused function

--- oly_matching/analyze.py
+++ oly_matching/analyze.py
@@ -9,15 +9,3 @@ def get_lis_id_with_n_types(df_matched: pd.DataFrame) -> pd.DataFrame:
         .apply(lambda x: x["N-Type No."].unique())
     )
     return lis_id_with_n_types
-
-
-def get_performance_per_model(df: pd.DataFrame) -> pd.DataFrame:
-    performance_per_model = df.groupby("model")["is_matched"].mean().round(2) * 100
-    matched_per_model = df.groupby("model")["is_matched"].sum()
-    types_per_model = df.grupby("model").size()
-    types_per_model.name = "count unique ids"
-    performance_per_model.name = "% ids matched"
-    matched_per_model.name = "# ids matched"
-    df_performance = pd.concat([matched_per_model, performance_per_model, types_per_model], axis=1)
-    df_performance = df_performance.sort_values(by="count unique ids", ascending=False)
-    return df_performance
